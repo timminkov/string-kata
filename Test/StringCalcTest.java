2fbfe0d2a69eab7131b8f1d53f4c9a84c7690639
Tim Minkov <timothyminkov@gmail.com>
2013-08-21 15:50:28 -0500

    Horrible code. Tests passing but went down the wrong path.

--- Test/StringCalcTest.java
+++ Test/StringCalcTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -11,9 +12,41 @@ import static org.junit.Assert.assertThat;
  * To change this template use File | Settings | File Templates.
  */
 public class StringCalcTest {
+    private StringCalc calc;
+
+    @Before
+    public void setUp() {
+        calc = new StringCalc();
+    }
+
     @Test
     public void shouldReturn0ForEmptyString() {
-        StringCalc calc = new StringCalc();
         assertThat(calc.add(""), is(0));
     }
+
+    @Test
+    public void shouldReturn1For1() {
+        assertThat(calc.add("1"), is(1));
+    }
+
+    @Test
+    public void shouldReturn3For1And2() {
+        assertThat(calc.add("1,2"), is(3));
+    }
+
+    @Test
+    public void shouldReturn5ForOneTwoAndTwo() {
+        assertThat(calc.add("1,2,2"), is(5));
+    }
+
+    @Test
+    public void shouldReturn6For123AndNewline() {
+        assertThat(calc.add("1\n2,3"), is(6));
+    }
+
+    @Test
+    public void changeDelimiterToPurple() {
+        assertThat(calc.add("[Purple]\n1Purple2Purple"), is(3));
+    }
+
 }
--- src/com.calc/StringCalc.java
+++ src/com.calc/StringCalc.java
@@ -1,3 +1,8 @@
+import sun.misc.Regexp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Pattern;
 
 /**
  * Created with IntelliJ IDEA.
@@ -8,6 +13,24 @@
  */
 public class StringCalc {
     public int add(String numbers) {
-        return 0;
+        String delimiter = new String("(\\D+)");
+        String comparison = new String();
+        if (numbers.startsWith("[")) {
+
+        }
+        else if (numbers.startsWith("\\D")) {
+
+        }
+        try {
+            int sum = 0;
+            String[] moreNumbers = (numbers.split("(\\D+)"));
+            for (String number : moreNumbers) {
+                sum += Integer.parseInt(number);
+            }
+            return sum;
+        }
+        catch (NumberFormatException num) {
+            return 0;
+        }
     }
 }
